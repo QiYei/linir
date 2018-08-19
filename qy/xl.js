4bd62bacc7a1e36e96a4d55a9b8d8c3a3724fa95
柒夜 <33372278+919293@users.noreply.github.com>
2018-08-20 02:25:41 +0800

    Update xl.js

--- qy/xl.js
+++ qy/xl.js
@@ -1,2 +1,5 @@
-<script> var xlm_wid='13632'; var xlm_url='https://www.xianliao.me/';</script>
+<script> 
+  var xlm_wid='13632'; 
+  var xlm_url='https://www.xianliao.me/';
+</script>
 <script type='text/javascript' charset='UTF-8' src='https://www.xianliao.me/embed.js'></script>
